996462304342dfd2a25a13681a917bcc94609b8b
Keshav <keshavsainiofficial@gmail.com>
2024-12-05 16:46:25 +0530

    add: github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kesavsaini.github.io',
+  base: '/MVPAgency',
   integrations: [tailwind()]
 });
