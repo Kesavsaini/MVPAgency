87312407015e93b49c7c96b1b730d5e7108577b0
Keshav <keshavsainiofficial@gmail.com>
2024-12-05 17:25:23 +0530

    fix: no-style issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kesavsaini.github.io',
-  base: 'my-repo',
+  base: '/MVPAgency',
   integrations: [tailwind()]
 });
